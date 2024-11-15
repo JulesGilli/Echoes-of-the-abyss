6cba5372e0f86fbf872baa769b6cb9fcbce53440
Basso Dev <sendtomadgab@gmail.com>
2024-11-15 12:06:06 +0100

    [EDIT] fix reset bonus effect of item at the die of player

--- core/src/main/java/io/github/maingame/itemManager/Gear.java
+++ core/src/main/java/io/github/maingame/itemManager/Gear.java
@@ -2,19 +2,8 @@ package io.github.maingame.itemManager;
 
 import io.github.maingame.characterManager.Entity;
 
-public abstract class Gear extends Item {
+public abstract class Gear extends Item implements GearManager{
     public Gear(int gold, String textureName, String textureBuyName) {
         super( gold, textureName, textureBuyName);
     }
-
-    public void applyItem(Entity target) {
-        {
-            if (this instanceof Weapon){
-                target.setAttackBonus(((Weapon) this).getAttackIncrease());
-            }
-        }
-            if (this instanceof Armor){
-                target.setArmor(((Armor) this).reductionDamage);
-            }
-        }
 }
