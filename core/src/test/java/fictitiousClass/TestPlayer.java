988b309d09f41e5190f11f7170e6441cdf03abda
Basso Dev <sendtomadgab@gmail.com>
2024-11-20 19:38:36 +0100

    [ADD] exec fon mac and linux
    testing exec, exec work

--- core/src/test/java/fictitiousClass/TestPlayer.java
+++ core/src/test/java/fictitiousClass/TestPlayer.java
@@ -5,6 +5,6 @@ import io.github.maingame.entities.Player;
 
 public class TestPlayer extends Player {
     public TestPlayer() {
-        super(new Vector2(0,0),null);
+        super(new Vector2(0,0),null,null);
     }
 }
