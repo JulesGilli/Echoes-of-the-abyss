644938a0ea1da774923a7797712c0e35efd4c94c
JulesGilli <jules.gilli@epitech.eu>
2024-11-22 14:21:34 +0100

    [EDIT] - fix gold text + equilibrage
    Fiw the text for the gold to be linked to the real gold droped by creature

--- core/src/main/java/io/github/maingame/entities/Enemy.java
+++ core/src/main/java/io/github/maingame/entities/Enemy.java
@@ -53,10 +53,11 @@ public abstract class Enemy extends Entity {
 
         this.attackCooldown = this.attackCooldown * multiplier;
 
+        this.gold = Math.round(this.gold * multiplier);
 
         this.speed = this.baseSpeed * (1 + (level - 1) * 0.1f);
 
-        float animationSpeedMultiplier = 1 / (1 + (level - 1) * 0.1f);
+        float animationSpeedMultiplier = 1 / (1 + (level - 1) / 1.05f);
         animation.updateFrameDurations(animationSpeedMultiplier);
     }
 
@@ -89,7 +90,7 @@ public abstract class Enemy extends Entity {
             if (health <= 0) {
                 isDying = true;
                 animationTime = 0f;
-                goldTexts.add(new GoldText("+10", new Vector2(position.x, position.y)));
+                goldTexts.add(new GoldText("" + this.gold, new Vector2(position.x, position.y)));
             } else {
                 isHit = true;
                 hitAnimationTime = 0f;
--- core/src/main/java/io/github/maingame/utils/GoldText.java
+++ core/src/main/java/io/github/maingame/utils/GoldText.java
@@ -7,11 +7,14 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.math.Vector2;
 
+import static java.awt.SystemColor.text;
+
 public class GoldText {
     private final Vector2 position;
     private final float duration = 1.0f;
     private final BitmapFont font;
     private float elapsedTime = 0f;
+    private String text;
 
     public GoldText(String text, Vector2 position) {
         this.position = new Vector2(position.x, position.y + 50);
@@ -23,6 +26,8 @@ public class GoldText {
         this.font = generator.generateFont(parameter);
         Color MY_GOLD = new Color(255 / 255f, 204 / 255f, 101 / 255f, 1);
 
+        this.text = text;
+
         this.font.setColor(MY_GOLD);
     }
 
@@ -32,7 +37,7 @@ public class GoldText {
     }
 
     public void render(SpriteBatch batch) {
-        font.draw(batch, "+10", position.x + 180, position.y + 180);
+        font.draw(batch, text, position.x + 180, position.y + 180);
     }
 
     public boolean isExpired() {
